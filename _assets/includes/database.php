8bd3075a3772a5a6e26c2c5cadf33a14d9ae9879
superbar13 <theobarc@gmail.com>
2024-09-13 16:06:01 +0200

    correction syntaxe database

--- _assets/includes/database.php
+++ _assets/includes/database.php
@@ -1,1 +1,19 @@
 <?php
+    require 'dbpass.php';
+
+    try
+    {
+        // Connexion à la base de données.
+        $dsn = 'mysql:host=' . $host . ';dbname=' . $password;
+        $pdo = new \PDO($dsn, $username, $password);
+        // Codage de caractères.
+        $pdo->exec('SET CHARACTER SET utf8');
+    // Gestion des erreurs sous forme d'exceptions.
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+    // Affichage de l'erreur.
+        die('Erreur : ' . $e->getMessage());
+    }
+?>
