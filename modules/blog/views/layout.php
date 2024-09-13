53fab9fcf28e36f0fc1fcbe28e7dcfbfedbd058f
Pierre André RIGAT <pierre-andre.rigat@etu.univ-amu.fr>
2024-09-13 14:59:50 +0200

    add layout.php

---
+++ modules/blog/views/layout.php
@@ -0,0 +1,27 @@
+<?php
+namespace blog\views;
+class Layout // La classe Layout nous permet de poser la base de nos pages HTML
+{
+  public function __construct(private string $title, private string $content){} // On définit notre constructeur
+  public function show():void{ // Création de la fonction show, qui permet d'afficher le contenu de la page
+
+  ?>
+<!DOCTYPE html>  <!-- Structure basique de HTML, qui se retrouvera sur toutes les pages -->
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+    <title><?= $this->title; ?></title> <!-- Prend la valeur de $title -->
+    <link href="../../../_assets/styles/light-style.css" rel="stylesheet"/> <!-- Lien jusqu'au CSS -->
+</head>
+<body>
+<?= $this->content; ?> <!-- Contenu de la page, avec $content-->
+
+</body>
+
+</html>
+<?php
+  }
+
+
+}
+?>
