2667dfed0818377151ce77317633158db37aba5b
GRIMAUD-Estelle-2326056aa <estelle.grimaud@etu.univ-amu.fr>
2024-09-19 15:02:56 +0200

    Test redirection index.php avec action sur plats.php

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 require '_assets/includes/autoloader.php';
 
 try{
+    if (filter_input(INPUT_GET, 'action')){
+        if ($_GET['action'] == 'plats'){
+            (new \blog\controllers\plats())->execute();
+        }
+    }
     (new \blog\controllers\homepage())->execute();
 }
 catch(Exception $e){
