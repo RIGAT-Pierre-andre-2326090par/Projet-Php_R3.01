e46cd446a3144d82487292455f9f699c438806a3
GRIMAUD-Estelle-2326056aa <estelle.grimaud@etu.univ-amu.fr>
2024-09-19 14:31:49 +0200

    Ajout html dans la vue du homepage.php

--- modules/blog/views/homepage.php
+++ modules/blog/views/homepage.php
@@ -5,6 +5,9 @@ class homepage {
         ob_start();
 ?>
         <h1> Bienvenue sur Tenrac-Lovers </h1>
+        <p> Découvre une myriade de plats plus gras les uns que les autres ! </p>
+        <h2> De superbes plats </h2>
+        <button>  Voir plus </button>
  <?php
         (new \blog\views\layout('Accueil', ob_get_clean()))->show();
     }
