8f506841a633d82681f5e3b3d523a3c1d85e0c74
Théo BARCELO <theo.barcelo@etu.univ-amu.fr>
2024-09-21 17:33:04 +0200

    correction de l'autoloader avec _assets

--- _assets/includes/autoloader.php
+++ _assets/includes/autoloader.php
@@ -8,7 +8,15 @@ spl_autoload_register(function ($class) {
     if (file_exists($file)) {
         include $file;
     } else {
-        // Log or echo for debugging
-        error_log("Autoload failed for class: $class");
+        $baseDir = str_replace(DIRECTORY_SEPARATOR.'_assets'.DIRECTORY_SEPARATOR.'includes', '', __DIR__) . DIRECTORY_SEPARATOR .'_assets' . DIRECTORY_SEPARATOR;
+        $file = $baseDir . $class . '.php';
+
+        if (file_exists($file)) {
+            include $file;
+        } else {
+            echo "non";
+            // Log or echo for debugging
+            error_log("Autoload failed for class: $class");
+        }
     }
 });
