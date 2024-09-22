7dc7b9fcbb4cb997120d587e6f8038d286053d80
GRIMAUD-Estelle-2326056aa <estelle.grimaud@etu.univ-amu.fr>
2024-09-22 10:01:29 +0200

    Test page ordre.php avec le modèle MVC

--- modules/blog/views/ordre.php
+++ modules/blog/views/ordre.php
@@ -4,25 +4,25 @@ namespace blog\views;
 
 class ordre
 {
-    public function show():void{
+    public function show($clubs):void{
         ?>
+
         <h1>Ordre</h1>
+
         <p>Description de dingo wow</p>
-        <h3>CLUBS</h3>
-        <section class="infoClub">
-            <img src="#" />
-            <p> Nom </p> <br>
-            <p> Adresse </p>
-        </section>
-        <section class="infoClub">
-            <img src="#" />
-            <p> Nom </p> <br>
-            <p> Adresse </p>
-        </section>
+
+        <h3>Clubs</h3>
+
+        <?php foreach ($clubs as $club) { ?>
+            <section class="infoClub">
+                <img src="<?= htmlspecialchars($club->getImage()); ?>" alt="<?= htmlspecialchars($club->getName()); ?>" style="max-width: 200px; height: auto;" />
+                <h3><?= htmlspecialchars($club->getName()); ?><em></h3>
+                <p><?= htmlspecialchars($club->getAddress()); ?><em></p>
+            </section>
+
         <?php
+        }
         (new layout('Ordre et clubs', ob_get_clean()))->show();
     }
-
-
 }
 ?>
