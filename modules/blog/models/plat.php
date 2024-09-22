3a0a5484b5f921e560b1d3c219d3004a33a25fc6
Baptiste GASTON <baptiste.gaston@etu.univ-amu.fr>
2024-09-22 11:32:28 +0200

    correctif model plat

--- modules/blog/models/plat.php
+++ modules/blog/models/plat.php
@@ -8,16 +8,14 @@ class plat
     public function getPlat(){
         $pdo = (new \includes\database())->getInstance();
         $stmt = $pdo->prepare('SELECT * FROM PLAT WHERE NOM_PL=:id');
+        if ($stmt->rowCount() === 0) {
+            return null; // Pas de résultat, retourne null
+        }
         try
         {
             $stmt->bindParam(':id', $nom);
             $stmt->execute();
         }
-        if ($stmt->rowCount() === 0) {
-            return null; // Pas de résultat, retourne null
-        }
-        // Récupération des résultats sous forme de tableau associatif
-        return $stmt->fetch(PDO::FETCH_ASSOC);
         catch (PDOException $e)
         {
             // Affichage de l'erreur et rappel de la requête.
@@ -25,5 +23,9 @@ class plat
             echo 'Requête : ', $sql, PHP_EOL;
             exit();
         }
+
+        // Récupération des résultats sous forme de tableau associatif
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+
     }
 }
