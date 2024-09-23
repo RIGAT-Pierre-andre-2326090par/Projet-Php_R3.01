1f802184bdfd8d1fdb08ef11a980519cefc82f5b
Xanooo <tamineha@yahoo.fr>
2024-09-23 16:42:29 +0200

    Ajout de la page login.

---
+++ modules/blog/views/login.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace blog\views;
+
+class login
+{
+    public function show(){
+        ob_start();
+        ?>
+        <h2> Connexion </h2>
+        <form action="#">
+
+
+
+
+
+        </form>
+
+}
+
+}
