bb223fc1883e073bd20ee7572eb5866fb0d1d177
Xanooo <tamineha@yahoo.fr>
2024-09-25 14:25:08 +0200

    Commentaires, correction du code.

--- modules/blog/models/Tenrac.php
+++ modules/blog/models/Tenrac.php
@@ -7,22 +7,25 @@ class Tenrac
     public function __construct(){
 
     }
-    public function show()
+    public function show(): void
     {
 
     }
 
+    // FOnction nous permettant d'ajouter un utilisateur
     public function insertTenrac($nom, $email, $telephone, $adresse)
     {
         $sql = 'INSERT INTO TENRAC (NOM_TR, COURRIEL_TR, TELEPHONE_TR, ADRESSE_TR) VALUES (:nom, :email, :telephone, :adresse)';
 
     }
 
+    // Fonction nous permettant de changer certaines informations sur un utilisateur
     public function updateTenrac($nom, $email, $telephone, $adresse, $id_tr)
     {
         $sql = 'UPDATE TENRAC SET NOM_TR=:nom, COURRIEL_TR=:email, TELEPHONE_TR=:telephone, ADRESSE_TR=:adresse WHERE ID_TR=:id_tr';
     }
 
+    // Fonction qui nous permet de récupérer l'email d'un utilisateur
     public function getMail($email)
     {
         $pdo = (new \includes\database())->getInstance();
@@ -33,6 +36,7 @@ class Tenrac
         return $stmt->fetch();
     }
 
+    // Fonction nous permettant de supprimer un utilisateur
     public function deleteTenrac($id_tr){
         $sql = 'DELETE FROM TENRAC WHERE ID_TR = :id_tr';
     }
--- modules/blog/views/login.php
+++ modules/blog/views/login.php
@@ -4,21 +4,20 @@ namespace blog\views;
 
 class login
 {
-    public function show(){
+    public function show(): void{
         ob_start();
         ?>
-        <section class="formulaire">
+        <section class="formulaire"> <!-- On crée un formulaire afin de gérer la connexion à notre site -->
         <h2> Connexion </h2>
-        <form method="POST" action="index.php?action=login">
+        <form method="POST" action="../../../index.php?action=login"> <!-- La méthode POST nous permet de réobtenir les valeurs du formulaire -->
             <label for="email">Email</label>
             <input type="email" id="email" name="email" required>
             <label for="password">Mot de passe</label>
             <input type="password" id="password" name="password" required>
             <input type="submit" value="Se connecter">
         </section>
-        </form>
-<?php
-        (new layout('Connexion', ob_get_clean()))->show();
+        <?php
+        (new layout('Connexion', ob_get_clean()))->show(); // On affiche la page avec le titre 'Connexion'
     }
 
 }
