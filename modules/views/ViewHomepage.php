49377edcec7474feca1131b5f0ffe69e4cb81519
Théo BARCELO <theo.barcelo@etu.univ-amu.fr>
2024-09-27 10:53:19 +0200

    Modification affichage liste dans home

--- modules/views/ViewHomepage.php
+++ modules/views/ViewHomepage.php
@@ -10,10 +10,10 @@ class ViewHomepage {
             <p>Découvre une myriade de plats plus gras les uns que les autres !</p>
         </section>
         <h2>De superbes plats</h2>
-        <div class="liste">
+        <div class="listeHome">
         <?php foreach ($plats as $plat) { ?>
             <a href="/index.php?action=plat&nom=<?= urlencode($plat->getNom()); ?>">
-                <section class="plat">
+                <section class="platHome">
                     <img src="<?= htmlspecialchars('/_assets/images/plat/' . $plat->getImage()); ?>">
                     <div>
                         <h3><?= htmlspecialchars($plat->getNom()); ?></h3>
@@ -25,10 +25,10 @@ class ViewHomepage {
         </div>
         <a href="/index.php?action=plats"> Voir plus </a>
         <h2>Des clubs a votre portée</h2>
-        <div class="liste">
+        <div class="listeHome">
         <?php foreach ($clubs as $club) { ?>
             <a href="/index.php?action=club&id=<?= urlencode($club->getId()); ?>">
-                <section class="infoClub">
+                <section class="infoClubHome">
                     <img src="<?= htmlspecialchars('/_assets/images/club/' . $club->getImage()); ?>">
                     <div>
                         <h3><?= htmlspecialchars($club->getNom()); ?></h3>
