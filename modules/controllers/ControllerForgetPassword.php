7ea7a7cc9c57509aa00ec538110c6e858f42a9cf
Xanooo <tamineha@yahoo.fr>
2024-09-29 18:17:36 +0200

    Ajout du contrôleur/modèle pour changer le mot de passe.

--- modules/controllers/ControllerForgetPassword.php
+++ modules/controllers/ControllerForgetPassword.php
@@ -1,11 +1,39 @@
 <?php
 
 namespace controllers;
+use models\ModelTenrac;
 use views\ViewForgetPassword;
 
 class ControllerForgetPassword
 {
 public function execute (): void{
-    (new ViewForgetPassword())->show(); // Affiche la vue ForgetPassword
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = $_POST["email"];
+        $user = (new ModelTenrac())->getMail($email);
+        if ($user) {
+            $subject = 'Réinitialisation de votre mot de passe';
+            $message = 'Bonjour, \n\ Voici le lien afin de réinitialiser votre mot de passe :';
+            $headers = 'From: no-reply@tenraclovers.com' . "\r\n" . 'Reply-To: no-reply@tenraclovers.com' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+
+
+            if (mail($email, $subject, $message, $headers)) {
+                echo 'Un email de réinitialisation de mot de passe vous a été envoyé !';
+            } else{
+                echo "Echec de l'envoi de l'email. Veuillez réessayer ultérieurement.";
+            }
+
+        }
+        else{
+            echo "Aucun utilisateur n'utilise cette adresse. Veuillez réessayer.";
+        }
+    }
+    else{
+        (new ViewForgetPassword())->show(); // Affiche la vue ForgetPassword
+
+    }
+
+
+
+
 }
 }
---
+++ modules/views/ViewResetPassword.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace views;
+
+class ViewResetPassword
+{
+    public function show(): void{
+        ob_start();
+        ?>
+        <section class="formulaire"> <!-- On crée un formulaire afin de gérer la connexion à notre site -->
+            <h2> Connexion </h2>
+            <form method="POST" action="/index.php?action=forgetpassword"> <!-- La méthode POST nous permet de réobtenir les valeurs du formulaire -->
+                <label for="newMDP">Nouveau mot de passe</label>
+                <input type="password" id="newMDP" name="newMDP" required>
+                <label for="confirmMDP">Confirmer le nouveau mot de passe</label>
+                <input type="password" id="confirmMDP" name="confirmMDP" required>
+                <input type="submit" value="Valider"><br>
+        </section><!-- Une possibilité de retourner à la page de connexion -->
+        <?php
+        (new ViewLayout('Mot de passe oublié', ob_get_clean()))->show(); // On affiche la page avec le titre 'Connexion'
+    }
+
+}
+?>
