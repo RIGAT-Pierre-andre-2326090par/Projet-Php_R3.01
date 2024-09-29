17d3a8426f3a5c35ebd9279eb41b22d3955bee0d
Baptiste GASTON <baptiste.gaston@etu.univ-amu.fr>
2024-09-29 17:48:52 +0200

    ajout gestion erreur de login

--- modules/controllers/ControllerLogin.php
+++ modules/controllers/ControllerLogin.php
@@ -8,12 +8,19 @@ class ControllerLogin {
     }
 
     public function execute(): void {
+        $model=(new \models\ModelLogin());
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $email = $_POST['email'];
             $password = $_POST['password'];
-            (new \models\ModelLogin())->login($email, $password);
+            $model->login($email, $password);
         }
-        (new \views\ViewLogin())->show();
+        if($model->getNb()==1 || $model->getNb()==2){
+            (new \views\ViewLayout('Erreur',$model->getMessageError()))->show();
+        }
+        else{
+            (new \views\ViewLogin())->show();
+        }
+
 
     }
 
