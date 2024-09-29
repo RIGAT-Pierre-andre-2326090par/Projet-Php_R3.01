d705c1d910f12f29bddafa6a6d07a9202f406974
Xanooo <tamineha@yahoo.fr>
2024-09-29 19:16:25 +0200

    Vue indiquant que le mot de passe a été confirmé.

--- modules/controllers/ControllerForgetPassword.php
+++ modules/controllers/ControllerForgetPassword.php
@@ -1,39 +1,47 @@
 <?php
 
 namespace controllers;
+
 use models\ModelTenrac;
+use views\ViewError;
 use views\ViewForgetPassword;
+use views\ViewPasswordResetConfirmation;
 
 class ControllerForgetPassword
 {
-public function execute (): void{
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $email = $_POST["email"];
-        $user = (new ModelTenrac())->getMail($email);
-        if ($user) {
-            $subject = 'Réinitialisation de votre mot de passe';
-            $message = 'Bonjour, \n\ Voici le lien afin de réinitialiser votre mot de passe :';
-            $headers = 'From: no-reply@tenraclovers.com' . "\r\n" . 'Reply-To: no-reply@tenraclovers.com' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+    public function execute(): void
+    {
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $email = $_POST["email"];
 
+            // Validation du format de l'email
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
-            if (mail($email, $subject, $message, $headers)) {
-                echo 'Un email de réinitialisation de mot de passe vous a été envoyé !';
-            } else{
-                echo "Echec de l'envoi de l'email. Veuillez réessayer ultérieurement.";
+                return;
             }
 
-        }
-        else{
-            echo "Aucun utilisateur n'utilise cette adresse. Veuillez réessayer.";
+            // Recherche de l'utilisateur par e-mail
+            $user = (new ModelTenrac())->getMail($email);
+
+            // Message générique pour éviter de donner des informations sur l'existence de l'e-mail
+            $subject = 'Réinitialisation de votre mot de passe';
+            $message = "Bonjour,\n\nVoici le lien pour réinitialiser votre mot de passe : [lien ici]";
+            $headers = 'From: no-reply@tenraclovers.com' . "\r\n" .
+                'Reply-To: no-reply@tenraclovers.com' . "\r\n" .
+                'X-Mailer: PHP/' . phpversion();
+
+            if ($user) {
+                // Tentative d'envoi de l'e-mail
+                if (mail($email, $subject, $message, $headers)) {
+                    (new ViewPasswordResetConfirmation())->show();
+                }
+            } else {
+                // Toujours afficher un message générique, même si l'utilisateur n'existe pas
+                (new ViewPasswordResetConfirmation())->show();
+            }
+        } else {
+            // Si pas de soumission de formulaire, affichage du formulaire
+            (new ViewForgetPassword())->show();
         }
     }
-    else{
-        (new ViewForgetPassword())->show(); // Affiche la vue ForgetPassword
-
-    }
-
-
-
-
-}
 }
